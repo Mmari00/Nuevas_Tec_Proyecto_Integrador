99a536a63aa0444ac33b0129148bcfa2b133dd79
Mmari00 <1software.files1@gmail.com>
2025-05-15 23:28:43 -0500

    Filtros agregados

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-st.markdown("Proyecto Intregador")
+st.title("Proyecto Intregador")
 
 
 #DataFrame
-st.subheader("DataFrame")
+st.subheader("DataFrame: ")
 archivo_csv = 'static/datasets/tecnologia_registros.csv'
 dataframe_csv = pd.read_csv(archivo_csv)
 st.dataframe(dataframe_csv)
+
+
+st.subheader("Filtro por lenguaje: ")
+lenguaje = st.selectbox("Selecciona un lenguaje", dataframe_csv["Lenguaje favorito"].unique())
+df_lenguaje = dataframe_csv[dataframe_csv["Lenguaje favorito"] == lenguaje]
+st.write("Datos filtrados: ")
+st.dataframe(df_lenguaje)
+
+
+
+st.title("Análisis de Datos Tecnológicos")
+
+# Seleccionar una variable para visualizar
+opcion = st.selectbox("Seleccione una variable para visualizar", ["Edad", "Años de experiencia", "Salario actual (USD)"])
+
+# Generar el gráfico
+fig, ax = plt.subplots()
+sns.histplot(dataframe_csv[opcion], bins=20, kde=True, ax=ax)
+ax.set_title(f"Distribución de {opcion}")
+ax.set_xlabel(opcion)
+ax.set_ylabel("Frecuencia")
+st.pyplot(fig)
