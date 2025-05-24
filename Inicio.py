d0e8e8cf042408c78e20234ccbe8b0908dfc170e
Mmari00 <1software.files1@gmail.com>
2025-05-24 08:43:32 -0500

    holaaaa

--- Inicio.py
+++ Inicio.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
+import plotly.express as px # type: ignore
 
 st.set_page_config(
     page_title="Página Web Streamlit",
